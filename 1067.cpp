aae2117863cc2bde43c099888e277f72d387c00c
Iffat Zerin <zeriniffat2@gmail.com>
2019-10-21 12:13:34 +0600

    uri solution 1067

---
+++ 1067.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,i;
+    cin>>a;
+    for(i=1;i<=a;i++)
+    {
+       if(i%2==1)
+       {
+           cout<<i<<endl;
+       }
+    }
+    return 0;
+}
